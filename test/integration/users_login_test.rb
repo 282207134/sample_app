c7f787f18565eadfe21bc6b7fdea707e81587387
282207134 <liu282207134@yahoo.co.jp>
2026-02-22 21:49:44 +0900

    リスト 9.28

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -5,6 +5,17 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
   end
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not cookies[:remember_token].blank?
+  end
+  test "login without remembering" do
+    # Cookieを保存してログイン
+       log_in_as(@user, remember_me: '1')
+    # Cookieが削除されていることを検証してからログイン
+       log_in_as(@user, remember_me: '0')
+    assert cookies[:remember_token].blank?
+  end
 
   test "login with valid email/invalid password" do
     get login_path
