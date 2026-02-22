d3e0f5f31b72d61fe7439c307cce946cbbb3aa53
282207134 <liu282207134@yahoo.co.jp>
2026-02-22 20:57:10 +0900

    リスト 9.21

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,11 +22,13 @@ class User < ApplicationRecord
   # 永続的セッションのためにユーザーをデータベースに記憶する
    def remember
      self.remember_token = User.new_token
+     return false if remember_digest.nil?
      update_attribute(:remember_digest, User.digest(remember_token))
    end
 
   # 渡されたトークンがダイジェストと一致したらtrueを返す
    def authenticated?(remember_token)
+     return false if remember_digest.nil?
      BCrypt::Password.new(remember_digest).is_password?(remember_token)
    end
 
