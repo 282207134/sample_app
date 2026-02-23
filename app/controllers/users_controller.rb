8321a44882be6effcaa5932a590556bdbd605fff
282207134 <liu282207134@yahoo.co.jp>
2026-02-23 18:46:43 +0900

    リスト 10.34

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,6 +45,7 @@ class UsersController < ApplicationController
   # ログイン済みユーザーかどうか確認 
   def logged_in_user
     unless logged_in?
+      store_location
       flash[:danger] = "Please log in."
       redirect_to login_url, status: :see_other
     end
@@ -53,6 +54,6 @@ class UsersController < ApplicationController
   # 正しいユーザーかどうか確認
   def correct_user
     @user = User.find(params[:id])
-    redirect_to(root_url, status: :see_other) unless @user == current_user
+    redirect_to(root_url, status: :see_other) unless current_user?(@user)
   end
 end
