f01667ff8ba6f07746f54a51a0065532585ad160
282207134 <liu282207134@yahoo.co.jp>
2026-03-01 16:36:58 +0900

    リスト 11.30

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,10 +32,11 @@ class User < ApplicationRecord
     remember_digest || remember
   end
 
-  # 渡されたトークンがダイジェストと一致したらtrueを返す
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  # 渡されたトークンがダイジェストと一致したらtrueを返す（:remember や :activation に対応）
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   # ユーザーのログイン情報を破棄する
