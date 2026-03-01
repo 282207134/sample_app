235e0d5ca673652bc5f4b191b1d20e7f5f7c9ec8
282207134 <liu282207134@yahoo.co.jp>
2026-03-01 17:17:36 +0900

    リスト 11.39

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token
   before_save   :downcase_email
   before_create :create_activation_digest
+  after_create  :send_activation_email
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -44,6 +45,12 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, nil)
   end
 
+  # アカウントを有効にする（AccountActivationsController から呼ぶため public）
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
   private
 
   # メールアドレスをすべて小文字にする
@@ -55,5 +62,10 @@ class User < ApplicationRecord
   def create_activation_digest
     self.activation_token  = User.new_token
     self.activation_digest = User.digest(activation_token)
+  end
+
+  # 有効化用のメールを送信する（after_create から呼ばれる）
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
   end
 end
