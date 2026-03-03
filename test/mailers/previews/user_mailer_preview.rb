28469f47adc8af3fa562825c77507fd6b3b3b16a
282207134 <liu282207134@yahoo.co.jp>
2026-03-03 23:26:23 +0900

    Reset password缺失的修复

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -12,8 +12,13 @@ class UserMailerPreview < ActionMailer::Preview
   # Preview this email at
   # http://localhost:3000/rails/mailers/user_mailer/password_reset
   def password_reset
-    user = User.first    
-    user.reset_token = User.new_token    
+    user = User.first
+    user.reset_token = User.new_token
+    # 把 digest 写入数据库，这样点击预览邮件中的链接时 token 能通过校验
+    user.update_columns(
+      reset_digest: User.digest(user.reset_token),
+      reset_sent_at: Time.zone.now
+    )
     UserMailer.password_reset(user)
   end
 end
