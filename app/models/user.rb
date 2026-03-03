b0ff73744756b63a69ac5f26a1378514492e0c4a
282207134 <liu282207134@yahoo.co.jp>
2026-03-03 23:54:01 +0900

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -56,6 +56,18 @@ class User < ApplicationRecord
     reset_sent_at < 2.hours.ago
   end
 
+  # パスワード再設定の属性を設定する（PasswordResetsController から呼ぶため public）
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  # パスワード再設定のメールを送信する
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
   private
 
   # メールアドレスをすべて小文字にする
@@ -74,16 +86,5 @@ class User < ApplicationRecord
   def send_activation_email
     return if activated?
     UserMailer.account_activation(self).deliver_now
-  end
-
-  # パスワード再設定の属性を設定する
-  def create_reset_digest
-    self.reset_token = User.new_token
-      update_attribute(:reset_digest,  User.digest(reset_token))    
-      update_attribute(:reset_sent_at, Time.zone.now)  
-  end  
-  # パスワード再設定のメールを送信する  
-  def send_password_reset_email    
-    UserMailer.password_reset(self).deliver_now  
   end
 end
